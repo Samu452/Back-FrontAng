254ea74b83ebea4b2a5e5a9ce47d98b20e72e93c
Samu <samuel.fuentealba.caceres@gmail.com>
2024-08-25 19:27:02 -0400

    practicaback&frontAngular11

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,43 @@
+// src/components/Header.js
+
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css"; // Asegúrate de tener el archivo de estilos
 
-const Header = () => (
-  <header>
-    <h1>Sammy's Store</h1>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
-      <Link to="/cart">Cart</Link>
-      <Link to="/profile">Profile</Link>
-    </nav>
-  </header>
-);
+function Header() {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link to="/" className="brand-link">
+            Sammy's Store
+          </Link>
+        </div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/products" className="nav-link">
+              Products
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/cart" className="nav-link">
+              Cart
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/profile" className="nav-link">
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
